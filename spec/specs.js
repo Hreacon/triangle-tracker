38dc043a4dd8ebfb8bb5662b79f3cce5d57999ec
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-19 10:10:08 -0800

    add spec for isosceles triangle. Fail.

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,7 @@ describe('triangleTracker', function() {
   it("is equilateral if all the inputs are the same", function() {
     expect(triangleTracker(100, 100, 100)).to.equal('equilateral');
   });
+  it("is isosceles if two inputs are the same", function() {
+    expect(triangleTracker(100, 100, 50)).to.equal('isosceles');
+  });
 });
