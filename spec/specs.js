629c32ea88a288eba9c261b61ebe490a7ac916f5
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-19 10:45:15 -0800

    add spec for not a triangle. fails.

--- spec/specs.js
+++ spec/specs.js
@@ -8,4 +8,7 @@ describe('triangleTracker', function() {
   it("is scalene if no sides are the same", function() {
     expect(triangleTracker(40, 50, 60)).to.equal('scalene');
   });
+  it("is not a triangle if two sides are equal to or less than the third side", function(){
+    expect(triangleTracker(40, 50, 100)).to.equal('not a triangle');
+  })
 });
