55deac29408011a65385b199e6cf1a4c1ae092f7
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-19 10:25:24 -0800

    add funtion to make triangle tracker pass isosceles spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 
-var triangleTracker=function(){
+var triangleTracker=function(side1, side2, side3){
   var output="equilateral";
+   if(side1 === side2 || side1 === side3 || side2 === side3) {
+     if(side1 === side3 && side1 === side2) {
+       output="equilateral"
+     } else {
+       output="isosceles";
+     };
+   };
   return output;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -3,6 +3,6 @@ describe('triangleTracker', function() {
     expect(triangleTracker(100, 100, 100)).to.equal('equilateral');
   });
   it("is isosceles if two inputs are the same", function() {
-    expect(triangleTracker(100, 100, 50)).to.equal('isosceles');
+    expect(triangleTracker(100, 50, 50)).to.equal('isosceles');
   });
 });
