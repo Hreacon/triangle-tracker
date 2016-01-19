f6a39c6cfd8fb0304d086db75e0deb95df58ff84
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-01-19 11:17:01 -0800

    add check for string input to equal false

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 
 var triangleTracker=function(side1, side2, side3){
   var output="scalene";
-   if(side1 === side2 || side1 === side3 || side2 === side3) {
-
-     if(side1 === side3 && side1 === side2) {
-
-       output="equilateral";
-     } else {
-       output="isosceles";
-     }
-
-   }
-   if(side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
-     output = "not a triangle";
-   }
+  if(side1 === side2 || side1 === side3 || side2 === side3) {
+    if(side1 === side3 && side1 === side2) {
+      output="equilateral";
+    } else {
+      output="isosceles";
+    }
+  }
+  if(side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
+    output = "not a triangle";
+  }
+  if(isNaN(side1) || isNaN(side2) || isNaN(side3)) {
+    output = false;
+  }
   return output;
 }
